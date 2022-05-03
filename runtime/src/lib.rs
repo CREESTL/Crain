d8f2a28706070dff59a88e14b7af172e69376749
CREESTL <kroymw3@yandex.ru>
2022-05-03 22:49:03 +0300

    Change `pallet_transaction_payment` Implementation for Runtime for Up-To-Date

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -248,9 +248,9 @@ parameter_types! {
 
 impl pallet_transaction_payment::Config for Runtime {
 	type OnChargeTransaction = CurrencyAdapter<Balances, ()>;
-	type TransactionByteFee = TransactionByteFee;
 	type OperationalFeeMultiplier = ConstU8<5>;
 	type WeightToFee = IdentityFee<Balance>;
+	type LengthToFee = IdentityFee<Balance>;
 	type FeeMultiplierUpdate = ();
 }
 
