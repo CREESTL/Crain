5ec696f534a3aefabdf68c02fd9d0e10f91f7a66
CREESTL <kroymw3@yandex.ru>
2022-06-09 18:32:02 +0300

    PostBox Does Not Build due to `doesn't have a size known at compile-time` Error

--- contracts/postbox/src/lib.rs
+++ contracts/postbox/src/lib.rs
@@ -2,40 +2,49 @@
 
 use ink_lang as ink;
 
-
 /*
-A time-limited auction contract
+An auction contract
 */
 #[ink::contract]
 mod postbox {
 
     use ink_storage::traits::SpreadAllocate;
-    use ink_env::DefaultEnvironment;
+    use ink_env::debug_println;
+    use rand::Rng;
 
     #[ink(storage)]
     #[derive(SpreadAllocate)]
     pub struct Postbox {
         // Number of members
         size: u32,
-        // Opening time
-        open_time: u64,
-        // Closing time
-        close_time: u64,
+        // How many bids have been made
+        bids_made: u32,
+        // Accounts of members;
+        members: Vec<AccountId>,
         // Bids of members
-        bids: ink_storage::Mapping<AccountId, u32>,
+        // NOTE Does not implement Iterator!
+        bids: ink_storage::Mapping<AccountId, Balance>,
     }
 
+    #[ink(event)]
+    pub struct BidMade {
+        from: Option<AccountId>,
+        value: Balance,
+    }
     impl Postbox {
         #[ink(constructor)]
-        pub fn new(first_bid: u32) -> Self {
+        pub fn new(first_bid: Balance) -> Self {
+
+        debug_println!("Initializing contract with {} tokens", first_bid);
         ink_lang::utils::initialize_contract(
                         |contract: &mut Self| {
                             // Max 5 members
                             contract.size = 5;
-                            contract.open_time = ink_env::block_timestamp::<DefaultEnvironment>();
-                            // Auction lasts 1 hour
-                            contract.close_time =ink_env::block_timestamp::<DefaultEnvironment>() + 3600 * 1000;
+                            // One bid is done in start
+                            contract.bids_made = 1;
                             let caller = Self::env().caller();
+                            // Add caller to the members
+                            contract.members.push(caller);
                             // Initialize it with the first bid on start
                             contract.bids.insert(&caller, &first_bid);
                         }
@@ -44,17 +53,67 @@ mod postbox {
 
         // Function to add a bid
         #[ink(message)]
-        pub fn add_bid(&mut self, amount: u32) {
+        #[ink(payable)]
+        pub fn add_bid(&mut self, amount: Balance) {
             let caller = self.env().caller();
             self.bids.insert(&caller, &amount);
+            self.members.push(caller);
+            self.bids_made += 1;
+            self.env().emit_event(BidMade{
+                from: Some(self.env().caller()),
+                value: amount
+            });
+            if self.bids_made == self.size {
+               self.give_prize();
+            }
+
+        }
+
+        // Function returns the total sum of bids
+        fn get_total_sum(&self) -> Balance {
+            let mut total: Balance = 0;
+            for member in self.members.iter() {
+                let bid = self.bids.get(member).unwrap();
+                total += bid;
+            }
+
+            total
+        }
+
+        // Function transfers all funds to the winner
+        fn give_prize(&mut self) {
+            let total = self.get_total_sum();
+            let for_each = total as u32 / self.size;
+            debug_println!("for each part is {}", for_each);
+            debug_println!("total balance of contract is {}", self.env().balance());
+            let winner = self.find_winner();
+            if self.env().transfer(winner, for_each.into()).is_err() {
+                panic!("Prize transfer failed!");
+            }
+
+        }
+
+        // Function generated a random number of winner
+        fn find_winner(&self) -> AccountId {
+            let mut rng = rand::thread_rng();
+            let winner_pos = rng.gen_range(0..self.size + 1);
+            let winner = self.members.get(winner_pos as usize).unwrap();
+            *winner
         }
 
         // Function returns the bid of the caller
         #[ink(message)]
-        pub fn get_bid(&self) -> Option<u32> {
+        pub fn get_bid(&self) -> Option<Balance> {
             let caller = self.env().caller();
             self.bids.get(&caller)
         }
+
+        // Function returns the number of bids made
+        #[ink(message)]
+        pub fn get_bids_made(&self) -> u32 {
+            self.bids_made
+        }
+
     }
 
     // TODO add tests here
