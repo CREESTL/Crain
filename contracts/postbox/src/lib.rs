cde61f030b3d12a2294495d5893d766fd6aa3c57
CREESTL <kroymw3@yandex.ru>
2022-06-10 22:55:44 +0300

    Add Random Number Generator. Add Checks. Add Events.

--- contracts/postbox/src/lib.rs
+++ contracts/postbox/src/lib.rs
@@ -10,10 +10,13 @@ mod postbox {
 
     use ink_storage::traits::SpreadAllocate;
     use ink_env::debug_println;
-    // use rand::{Rng, SeedableRng};
-    // use rand_chacha::*;
-    use byteorder::{BigEndian, ReadBytesExt}; // 1.2.7
-
+    use rand_chacha::ChaCha20Rng;
+    use rand_chacha::rand_core::SeedableRng;
+    use rand_chacha::rand_core::RngCore;
+    use scale::{
+        Decode,
+        Encode,
+    };
 
     #[ink(storage)]
     #[derive(SpreadAllocate)]
@@ -35,6 +38,18 @@ mod postbox {
         value: Balance,
     }
 
+    #[ink(event)]
+    pub struct MaxBidsReached {}
+
+    #[ink(event)]
+    pub struct PrizeTransfered {}
+
+    #[derive(Encode, Decode, Debug, PartialEq, Eq, Copy, Clone)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Error {
+        MaxBids,
+        DuplicateMember,
+    }
 
     impl Postbox {
         #[ink(constructor)]
@@ -59,8 +74,20 @@ mod postbox {
         // Function to add a bid
         #[ink(message)]
         #[ink(payable)]
-        pub fn add_bid(&mut self, amount: Balance) {
+        pub fn add_bid(&mut self, amount: Balance) -> Result <(), Error> {
+            if self.bids_made == self.size {
+                // Emit the event for log first
+                self.env().emit_event(MaxBidsReached{});
+                // Give the prize to the winner
+                self.give_prize();
+                // Return error and do not continue
+                return Err(Error::MaxBids);
+            }
             let caller = self.env().caller();
+            // Same member can't bid more then once
+            if self.members.contains(&caller) {
+                return Err(Error::DuplicateMember);
+            }
             self.bids.insert(&caller, &amount);
             self.members.push(caller);
             self.bids_made += 1;
@@ -68,11 +95,9 @@ mod postbox {
                 from: Some(self.env().caller()),
                 value: amount
             });
-            if self.bids_made == self.size {
-               self.give_prize();
-            }
-
+            Ok(())
         }
+
 
         // Function returns the total sum of bids
         fn get_total_sum(&self) -> Balance {
@@ -95,15 +120,25 @@ mod postbox {
             if self.env().transfer(winner, for_each.into()).is_err() {
                 panic!("Prize transfer failed!");
             }
+            self.env().emit_event(PrizeTransfered {});
 
         }
 
         // Function generated a random number of winner
         fn find_winner(&self) -> AccountId {
-            //let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(10);
-            //let winner_pos = rng.gen_range(0..=10);
+            // Use AccountId of the caller to add more randomness
+            let winner_pos = self.gen_range(0, self.size as u64, self.env().caller());
             let winner = self.members.get(winner_pos as usize).unwrap();
             *winner
+        }
+
+        // Function generates a random number in a given range
+        fn gen_range(&self, min: u64, max: u64, user_account: AccountId) -> u32 {
+            let random_seed = self.env().random(user_account.as_ref());
+            let mut seed_converted: [u8; 32] = Default::default();
+            seed_converted.copy_from_slice(random_seed.0.as_ref());
+            let mut rng = ChaCha20Rng::from_seed(seed_converted);
+            ((rng.next_u64() / u64::MAX) * (max - min) + min) as u32
         }
 
         // Function returns the bid of the caller
@@ -119,6 +154,20 @@ mod postbox {
             self.bids_made
         }
 
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        use ink_lang as ink;
+
+        /// We test if the default constructor does its job.
+        #[ink::test]
+        fn new_works() {
+            let flipper = Flipper::default();
+            assert_eq!(flipper.get(), false);
+        }
     }
 
     // TODO add tests here
